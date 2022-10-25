98c8b4316f23bc46a6c00f6ff52b00fae201e0a4
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2022-10-25 09:51:35 +0200

    Ruta de página inicial

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('entradas.index');
 });
 
 Route::resource('entradas', EntradaController::class);
