b6a249014abb619cfed6ac5aee1cade9bcf0f179
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2022-11-04 14:15:47 +0100

    CRUD básico de comentarios

--- app/Http/Controllers/ComentarioController.php
+++ app/Http/Controllers/ComentarioController.php
@@ -8,79 +8,56 @@ use App\Models\Comentario;
 
 class ComentarioController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
-        //
+        $comentarios = Comentario::all();
+
+        return view('comentarios.index', compact('comentarios'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
-        //
+        return view('comentarios.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \App\Http\Requests\StoreComentarioRequest  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(StoreComentarioRequest $request)
     {
-        //
+        Comentario::create([
+            'email' => request('email'),
+            'texto' => request('texto'),
+            'fecha' => request('fecha'),
+            'visible' => $request->has('visible'),
+        ]);
+
+        return redirect(route('comentarios.index'));
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Comentario  $comentario
-     * @return \Illuminate\Http\Response
-     */
     public function show(Comentario $comentario)
     {
-        //
+        return view('comentarios.show', compact('comentario'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Comentario  $comentario
-     * @return \Illuminate\Http\Response
-     */
     public function edit(Comentario $comentario)
     {
-        //
+        return view('comentarios.edit', compact('comentario'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \App\Http\Requests\UpdateComentarioRequest  $request
-     * @param  \App\Models\Comentario  $comentario
-     * @return \Illuminate\Http\Response
-     */
     public function update(UpdateComentarioRequest $request, Comentario $comentario)
     {
-        //
+        $comentario->update([
+            'email' => request('email'),
+            'texto' => request('texto'),
+            'fecha' => request('fecha'),
+            'visible' => $request->has('visible'),
+        ]);
+
+        return redirect(route('comentarios.index'));
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Comentario  $comentario
-     * @return \Illuminate\Http\Response
-     */
     public function destroy(Comentario $comentario)
     {
-        //
+        $comentario->delete();
+
+        return back();
     }
 }
